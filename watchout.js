b2d8f776d94e784b1c85cd5a740d30e0a92fcc13
nmandel <nrmandel@ucdavis.edu>
2014-11-07 14:47:37 -0800

    drew enemies in SVG element

--- watchout.js
+++ watchout.js
@@ -1,2 +1,22 @@
 // start slingin' some d3 here.
 
+var numEnemies = 30;
+var enemyArray = [];
+
+for (var i = 0; i < numEnemies; i++){
+  enemyArray.push(
+     {
+      id: i,
+      cx: Math.random()*parseInt(d3.select('body').select('svg').style("width")),
+      cy: Math.random()*parseInt(d3.select('body').select('svg').style("height"))
+     }
+    )
+}
+
+for (var j = 0; j < enemyArray.length; j++){
+  d3.select('body').select('svg').append("circle")
+  .attr("cx", enemyArray[j].cx)
+  .attr("cy", enemyArray[j].cy)
+  .attr("r", 10)
+  .style("fill", "green");
+}
