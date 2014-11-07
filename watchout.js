662d41a55110bc480716a2066a4efe751f4bf35b
nmandel <nrmandel@ucdavis.edu>
2014-11-07 15:46:22 -0800

    creates draggable player dot

--- watchout.js
+++ watchout.js
@@ -1,5 +1,31 @@
 // start slingin' some d3 here.
 
+//Global Variables
+
+var boardWidth = parseInt(d3.select('body').select('svg').style("width"));
+var boardHeight = parseInt(d3.select('body').select('svg').style("height"));
+
+// Player Movement
+var dragmove = function(d){
+  d3.select('.player')
+    .attr("cx", d3.event.x)
+    .attr("cy", d3.event.y)
+};
+
+var drag = d3.behavior.drag()
+    .on("drag", dragmove);
+
+// Create Player
+d3.select('body').select('svg').append("circle")
+  .attr("cx", boardWidth/2)
+  .attr("cy", boardHeight/2)
+  .attr("r", 8)
+  .attr("class", "player")
+  .style("fill", "orange")
+  .call(drag);
+
+
+// Create Enemies
 var numEnemies = 50;
 
 var generateEnemyArray = function () {
@@ -8,8 +34,8 @@ var generateEnemyArray = function () {
     enemyArray.push(
        {
         id: i,
-        cx: Math.random()*parseInt(d3.select('body').select('svg').style("width")),
-        cy: Math.random()*parseInt(d3.select('body').select('svg').style("height"))
+        cx: Math.random()*boardWidth,
+        cy: Math.random()*boardHeight
        }
       );
   }
@@ -22,12 +48,13 @@ for (var j = 0; j < enemyArray.length; j++){
   .attr("cx", enemyArray[j].cx)
   .attr("cy", enemyArray[j].cy)
   .attr("r", 10)
+  .attr("class", "enemy")
   .style("fill", "green");
 }
 
 
 var repeat = function() {
-  d3.selectAll("circle")
+  d3.selectAll(".enemy")
     .data(generateEnemyArray())
     .transition()
       .duration(1000)
