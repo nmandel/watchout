277e881a433fbffa6888436c2db7f66fb48e0685
nmandel <nrmandel@ucdavis.edu>
2014-11-07 20:31:53 -0800

    work on collisions

--- watchout.js
+++ watchout.js
@@ -4,9 +4,32 @@
 
 var boardWidth = parseInt(d3.select('body').select('svg').style("width"));
 var boardHeight = parseInt(d3.select('body').select('svg').style("height"));
+var collided = false;
+
+
+// Collision Handling
+var collide = function(enemy) {
+  var enemies = d3.select('.enemy');
+  for (var i = 0; i < enemies.length; i++) {
+    var player = d3.select('.player');
+    var xDiff = Math.abs(parseInt(player.attr("cx")) - parseInt(enemies[i].attr("cx");
+    var yDiff = Math.abs(parseInt(player.attr("cy")) - parseInt(enemies[i].atcy);
+    var bothRadiuses = parseInt(player.attr("r")) + d.r;
+    var distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+
+    // console.log(player.attr("cx") + " "  + enemy.cx + " " + xDiff);
+    // console.log(distance);
+    if (distance <= bothRadiuses) {
+      console.log("BOOOOOM!");
+      collided = true;
+    }
+  });
+
+};
 
 // Player Movement
-var dragmove = function(d){
+var dragmove = function(){
+  collide();
   d3.select('.player')
     .attr("cx", d3.event.x)
     .attr("cy", d3.event.y)
@@ -19,20 +42,21 @@ var drag = d3.behavior.drag()
 d3.select('body').select('svg').append("circle")
   .attr("cx", boardWidth/2)
   .attr("cy", boardHeight/2)
-  .attr("r", 8)
+  .attr("r", 30)
   .attr("class", "player")
   .style("fill", "orange")
   .call(drag);
 
 
 // Create Enemies
-var numEnemies = 50;
+var numEnemies = 1;
 
 var generateEnemyArray = function () {
   var enemyArray = [];
   for (var i = 0; i < numEnemies; i++){
     enemyArray.push(
        {
+        r: 50,
         id: i,
         cx: Math.random()*boardWidth,
         cy: Math.random()*boardHeight
@@ -47,9 +71,8 @@ for (var j = 0; j < enemyArray.length; j++){
   d3.select('body').select('svg').append("circle")
   .attr("cx", enemyArray[j].cx)
   .attr("cy", enemyArray[j].cy)
-  .attr("r", 10)
+  .attr("r", 50)
   .attr("class", "enemy")
-  .style("fill", "green");
 }
 
 
@@ -57,7 +80,7 @@ var repeat = function() {
   d3.selectAll(".enemy")
     .data(generateEnemyArray())
     .transition()
-      .duration(1000)
+      .duration(5000)
     .attr("cx", function(d) {
       return d.cx;
     })
@@ -67,3 +90,13 @@ var repeat = function() {
     .each("end", repeat);
 };
 repeat();
+
+setInterval(collide, 200);
+
+// function collisionChecker () {
+//   d3.selectAll(".enemy")
+//     .each(function(d) {
+//       // console.log(d);
+//       collide(d);
+//     });
+// };
