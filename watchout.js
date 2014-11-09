533efe7bbdf5e2e7bc657b7100ae5bd59d95c861
nmandel <nrmandel@ucdavis.edu>
2014-11-08 16:02:28 -0800

    gives images to player, dead player, enemies and background, spins enemies

--- watchout.js
+++ watchout.js
@@ -4,15 +4,18 @@
 
 var boardWidth = parseInt(d3.select('body').select('svg').style("width"));
 var boardHeight = parseInt(d3.select('body').select('svg').style("height"));
-var numEnemies = 20;
+var numEnemies = 25;
 var enemySize = 20;
 var numCollisions = 0;
 var score = 0;
 var highScore = 0;
+var speed = 3000;
 var collided;
+var rotateEnemy = 0;
 
 // Scoreboard
 var incrementCollisions = function(){
+  d3.select('.player').attr("xlink:href", "images/ghost.png");
   if(score > highScore){
     highScore = score;
     d3.select('.high').select('span').text(highScore);
@@ -22,13 +25,14 @@ var incrementCollisions = function(){
   d3.select('.collisions').select('span').text(numCollisions);
   setTimeout(function() {
     collided = false;
+    d3.select('.player').attr("xlink:href", "images/koala.png");
   }, 1000);
 }
 
 setInterval(function(){
   score++;
   d3.select('.current').select('span').text(score);
-}, 250);
+}, 100);
 
 // Collision Handling
 var collide = function(enemy) {
@@ -36,8 +40,8 @@ var collide = function(enemy) {
   for (var i = 0; i < enemies[0].length; i++) {
     var enemy = d3.select('.enemy'+i);
     var player = d3.select('.player');
-    var xDiff = Math.abs(parseInt(player.attr("cx")) - parseInt(enemy.attr("cx")));
-    var yDiff = Math.abs(parseInt(player.attr("cy")) - parseInt(enemy.attr("cy")));
+    var xDiff = Math.abs(parseInt(player.attr("x")) - 5 - parseInt(enemy.attr("x")));
+    var yDiff = Math.abs(parseInt(player.attr("y")) - 5 - parseInt(enemy.attr("y")));
     var bothRadiuses = parseInt(player.attr("r")) + parseInt(enemy.attr("r"));
     var distance = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
     // console.log(player.attr("cx") + " " + player.attr("cy"));
@@ -55,20 +59,25 @@ var collide = function(enemy) {
 var dragmove = function(){
   collide();
   d3.select('.player')
-    .attr("cx", d3.event.x)
-    .attr("cy", d3.event.y)
+    .attr("x", d3.event.x)
+    .attr("y", d3.event.y);
 };
 
 var drag = d3.behavior.drag()
     .on("drag", dragmove);
 
 // Create Player
-d3.select('body').select('svg').append("circle")
-  .attr("cx", boardWidth/2)
-  .attr("cy", boardHeight/2)
-  .attr("r", 15)
+d3.select('body').select('svg').append("image")
+  .attr("x", boardWidth/2)
+  .attr("y", boardHeight/2)
+  .attr("r", 25)
   .attr("class", "player")
-  .style("fill", "orange")
+  // .append("svg:image")
+  .attr("height", "50px")
+  .attr("width", "50px")
+  // .style("fill", "transparent")
+  .attr("xlink:href", "images/koala.png")
+  // .style("fill", "url(images/koala.png)")
   .call(drag);
 
 
@@ -90,27 +99,38 @@ var generateEnemyArray = function () {
 var enemyArray = generateEnemyArray();
 
 for (var j = 0; j < enemyArray.length; j++){
-  d3.select('body').select('svg').append("circle")
-  .attr("cx", enemyArray[j].cx)
-  .attr("cy", enemyArray[j].cy)
-  .attr("r", enemySize)
-  .attr("class", "enemy" + " enemy" + enemyArray[j].id);
+  d3.select('body').select('svg').append("image")
+    .attr("x", enemyArray[j].cx)
+    .attr("y", enemyArray[j].cy)
+    .attr("r", enemySize)
+    .attr("height", "40px")
+    .attr("width", "40px")
+    .attr("xlink:href", "images/boomerang.png")
+    .attr("class", "enemy" + " enemy" + enemyArray[j].id)
+    .style("position", "absolute")
+    .style("margin", "-20px 0 0 -20px")
+    .style("transform-origin", "center")
+    .style("-webkit-animation", "spin .6s linear infinite")
+    .style("-moz-animation", "spin .6s linear infinite")
+    .style("animation", "spin .6s linear infinite");
 }
 
 
+// Enemy movement
 var repeat = function() {
   d3.selectAll(".enemy")
     .data(generateEnemyArray())
     .transition()
-      .duration(5000)
-    .attr("cx", function(d) {
+      .duration(speed)
+    .attr("x", function(d) {
       return d.cx;
     })
-    .attr("cy", function(d) {
+    .attr("y", function(d) {
       return d.cy;
     })
     .each("end", repeat);
 };
+
 repeat();
 
 setInterval(collide, 100);
